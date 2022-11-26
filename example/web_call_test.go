53892418cab392e67ff0d7c008acbb47bddbd02e
Hugo Sousa <gi.ciberon@gmail.com>
2022-11-26 15:01:54 +0100

    Add comments

--- example/web_call_test.go
+++ example/web_call_test.go
@@ -3,6 +3,7 @@ package example
 import (
 	"context"
 	"errors"
+	"github.com/stretchr/testify/assert"
 	s "github.com/xplosunn/sturdy/shortsyntax"
 	"github.com/xplosunn/sturdy/sturdytest"
 	"io"
@@ -10,8 +11,10 @@ import (
 	"testing"
 )
 
+// TestWebCall checks that the WebCall method is resilient against failure before and after the http call was done
 func TestWebCall(t *testing.T) {
-	sturdytest.SturdyTest(t, sturdytest.ErrorBeforeFirstCallAndThenIncreaseAfterEachCall(), func(ctx context.Context) error {
+	b := sturdytest.ErrorBeforeFirstCallAndThenIncreaseAfterEachCall()
+	sturdytest.SturdyTest(t, b, func(ctx context.Context) error {
 		callResult, err := WebCall(ctx)
 		if err != nil {
 			return err
@@ -21,8 +24,10 @@ func TestWebCall(t *testing.T) {
 		}
 		return nil
 	})
+	assert.Empty(t, b.NonInjectedErrors())
 }
 
+// WebCall gets a random cat fact from the internet and returns it.
 func WebCall(ctx context.Context) (string, error) {
 	resp, err := s.S1(ctx, func() (*http.Response, error) {
 		return http.Get("https://catfact.ninja/fact")
